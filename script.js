c6dd9ada88376300232bcf984c15cf5f0c9da5b4
Mohanad Nassar <127761857+Mohanad-Nassar@users.noreply.github.com>
2023-08-24 20:56:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-var tabs = document.querySelectorAll('#tabs > ul > li > a');
+(function () {
+    'use strict';
 
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', selectTab)
 
-}
+    const tabs = document.querySelectorAll('#tabs > ul > li > a');
 
-function selectTab(event) {
-    event.preventDefault();
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', selectTab)
+    });
 
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].removeAttribute('class');
+    function selectTab(event) {
+        event.preventDefault();
+
+        tabs.forEach((tab)=> {
+            tab.removeAttribute('class');
+        });
+
+        event.target.className = 'active';
+
+        const thisTab = event.target.getAttribute('href');
+        const thisContent = document.querySelector(thisTab);
+        const oldContent = document.querySelector('.visible');
+
+        oldContent.className = "visuallyHidden";
+
+        oldContent.addEventListener('transitionend', function () {
+            oldContent.className = "hidden";
+
+            thisContent.className = "visible visuallyHidden";
+
+            setTimeout(function () {
+                thisContent.classList.remove('visuallyHidden');
+            }, 20);
+
+        }, { capture: false, once: true, passive: false });
+
+
     }
 
-    event.target.className = 'active';
-
-    var thisTab = event.target.getAttribute('href');
-    var thisContent = document.querySelector(thisTab);
-
-    var oldContent = document.querySelector('.visible');
-    oldContent.className = "visuallyHidden";
-    oldContent.addEventListener('transitionend', function () {
-        oldContent.className = "hidden";
-
-        thisContent.className ="visible visuallyHidden"; 
-
-        setTimeout(function(){
-            thisContent.classList.remove('visuallyHidden');
-        }, 20);
-
-    },{capture:false, once:true, passive:false});
-
-
-}
+}());
